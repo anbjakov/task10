61552169b12cce629e6f7ee2505d1db365a69e4f
Andrii Biakov <an.bjakov@gmail.com>
2022-08-15 11:00:18 +0300

    rename method yearSalary

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 class Employee {
     constructor(firstName,lastName,age,jobPosition,salary){
-        [this.firstName, this.lastName, this.age, this.jobPosition, this.salary] = arguments
+       // [this.firstName, this.lastName, this.age, this.jobPosition, this.salary] = arguments
+        this.firstName = firstName;
+        this.lastName =lastName;
+        this.age = age;
+        this.jobPosition = jobPosition;
+        this.salary = salary;
     }
     static vacationDays = 18;
     get fullName(){
         return `${this.firstName} ${this.lastName}`
     }
-    yearSalarry(){
+    getYearSalary(){
         const VACATION_MONTHS = 12;
         return this.salary*VACATION_MONTHS
     }
@@ -17,5 +22,5 @@ class Employee {
 let worker = new Employee('Peter','Griffin',40,'brewer','10');
 let engineer = new Employee('Hubert','Farnsworth',160,'mad scientist','20');
 
-console.log(`employee ${worker.fullName}. He earn per year: ${worker.yearSalarry()}`);
-console.log(`employee ${engineer.fullName}. He earn per year: ${engineer.yearSalarry()}`);
+console.log(`employee ${worker.fullName}. He earn per year: ${worker.getYearSalary()}`);
+console.log(`employee ${engineer.fullName}. He earn per year: ${engineer.getYearSalary()}`);
